3d5275c4940a941ed7fa197fb8bdaa23b39b6f86
SebaQuiros <sebaquiros@gmail.com>
2022-08-13 15:01:42 -0300

    login

--- src/main/java/com/seba/portfolio/controller/LoginController.java
+++ src/main/java/com/seba/portfolio/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.seba.portfolio.controller;
 import com.seba.portfolio.model.User;
 import com.seba.portfolio.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class LoginController {
     @Autowired
