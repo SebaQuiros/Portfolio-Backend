0bb50482519dc16bed42d1eddf9a49b0bdef2c4d
SebaQuiros <sebaquiros@gmail.com>
2022-08-16 12:46:14 -0300

    url

--- src/main/java/com/seba/portfolio/model/SkillSet.java
+++ src/main/java/com/seba/portfolio/model/SkillSet.java
@@ -9,20 +9,21 @@ import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+@Getter
+@Setter
 @Entity
 public class SkillSet {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String type;
     private String title;
-    @Column(length=1000)
+    @Column(length = 1000)
     private String description;
     private ArrayList<String> tools;
-    
-    public SkillSet () {
+
+    public SkillSet() {
     }
 
     public SkillSet(Long id, String type, String title, String description, ArrayList tools) {
@@ -32,5 +33,5 @@ public class SkillSet {
         this.description = description;
         this.tools = tools;
     }
-    
+
 }
